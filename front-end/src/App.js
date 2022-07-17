7ce3cbb3b57f0464d92defca47e928e836df86d8
Deep Adeshra <deep.adeshraa@gmail.com>
2022-07-17 01:47:58 -0300

    Frontend for kitchen and invoices

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ShowTours from './routes/tours/show_tours';
-import BookingConfirmation from './routes/tours/booking_confirmation';
+import ShowTours from './routes/show_tours';
+import BookingConfirmation from './routes/booking_confirmation';
+import Home from './routes/home';
+import Kitchen from './routes/kitchen';
+import Invoices from './routes/invoices';
+import MyOrders from './routes/my_orders';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<BookingConfirmation />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path='/book_tour' element={<BookingConfirmation />}></Route>
           <Route path='/show_tours' element={<ShowTours />}></Route>
+          <Route path='/kitchen' element={<Kitchen />}></Route>
+          <Route path='/invoices' element={<Invoices />}></Route>
+          <Route path='/my_orders' element={<MyOrders />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
