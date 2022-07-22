52c63be589003b2a982dc81d2c3c90a553a9cbbb
Deep Adeshra <deep.adeshraa@gmail.com>
2022-07-22 20:42:35 -0300

    add users report

--- front-end/src/routes/visualization/data_visualization.js
+++ front-end/src/routes/visualization/data_visualization.js
@@ -10,6 +10,10 @@ function DataVisualization(props) {
       <div className="d-flex">
         <iframe className="mx-auto border rounded" width="1000" height="650" src="https://datastudio.google.com/embed/reporting/0e4264a6-7a43-4f21-a13a-c11b8244808d/page/tEnnC" frameborder="0" allowfullscreen></iframe>
       </div>
+      <hr />
+      <div className="d-flex">
+        <iframe className="mx-auto border rounded" width="1000" height="650" src="https://datastudio.google.com/embed/reporting/cd16cd51-3b5b-4279-bd1b-a466c9a33366/page/tEnnC" frameborder="0" allowfullscreen></iframe>
+      </div>
     </div>
   )
 }
