4f8f95e50a306a5622c4923cd40a27b20c421343
sureja <dh276903@dal.ca>
2022-07-23 18:37:02 -0300

    some changes to admin

--- front-end/src/routes/visualization/data_visualization.js
+++ front-end/src/routes/visualization/data_visualization.js
@@ -1,7 +1,21 @@
 function DataVisualization(props) {
   return (
     <div className="container mt-5">
-      <h1 className="text-center">Data Visualization</h1>
+        <div className="col-md-12">
+        <h1 className="text-center">Data Visualization</h1>
+
+        <div className="btn-group btn-group-lg w-100 mt-5" role="group" aria-label="Basic example">
+          <a href="/manage_room" type="button" className="btn btn-secondary">Manage Rooms</a>
+          <a href="/userfeedback" type="button" className="btn btn-secondary">
+							See FeedBacks
+						</a>
+						<a href="/data_visualization" type="button" className="btn btn-secondary">
+							See Data visualization
+						</a>
+          <a href="/login" type="button" className="btn btn-secondary">Logout</a>
+        </div>
+      </div>
+     
       <hr />
       <div className="d-flex">
         <iframe className="mx-auto border rounded" width="1000" height="650" src="https://datastudio.google.com/embed/reporting/0e70c85b-f355-4676-8340-54e72d1c9110/page/tEnnC" frameborder="0" allowfullscreen></iframe>
