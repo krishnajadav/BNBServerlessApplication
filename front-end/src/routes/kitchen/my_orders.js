906e61fa7bbe466d31fb6182dbdfd154ba14004d
Manali Shah <manalishah@Manalis-MacBook-Pro.local>
2022-07-23 03:29:13 -0300

    kitchen UI changes

--- front-end/src/routes/kitchen/my_orders.js
+++ front-end/src/routes/kitchen/my_orders.js
@@ -15,7 +15,7 @@ function MyOrders(props) {
     let user = localStorage.getItem("user");
 
     if (!user) {
-      alert('Please login to view your tickets');
+      alert('Please login to view your orders');
       return;
     }
     user = JSON.parse(user);
@@ -25,7 +25,55 @@ function MyOrders(props) {
 
 
   return (
+    <div>
+   
     <div className="container mt-5">
+    <div className="container mt-5">
+			{/* Add button group  */}
+			<div className="row">
+				<div className="col-md-12">
+					<h1 className="text-center">Serverless Project: Group-11</h1>
+
+					<div className="btn-group btn-group-lg w-100 mt-5" role="group" aria-label="Basic example">
+						<a href="/kitchen" type="button" className="btn btn-secondary">
+							Kitchen
+						</a>
+						<a href="/search_room" type="button" className="btn btn-secondary">
+							Hotel
+						</a>
+						<a href="/book_tour" type="button" className="btn btn-secondary">
+							Tour
+						</a>
+						<a href="/invoices" type="button" className="btn btn-secondary">
+							My Bills
+						</a>
+						<a href="/my_orders" type="button" className="btn btn-secondary">
+							My Orders
+						</a>
+					</div>
+					<div className="btn-group btn-group-lg w-100 mt-5" role="group" aria-label="Basic example">
+						<a href="/my_reservation" type="button" className="btn btn-secondary">
+							My Room Bookings
+						</a>
+						<a href="/feedback" type="button" className="btn btn-secondary">
+							Write FeedBack
+						</a>
+						<a href="/my_tickets" type="button" className="btn btn-secondary">
+							My tour bookings
+						</a>
+						<a href="/userfeedback" type="button" className="btn btn-secondary">
+							See FeedBacks
+						</a>
+						<a href="/data_visualization" type="button" className="btn btn-secondary">
+							See Data visualization
+						</a>
+						<a href="/report" type="button" className="btn btn-secondary">
+							Access Report
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
       <h1 className="text-center">My Orders</h1>
       
       <div className="row">
@@ -37,6 +85,8 @@ function MyOrders(props) {
                   <div className="card-body">
                     <h5 className="card-title">Order #{index + 1}</h5>
                     <p className="card-text">
+                      <a style={{color:"green"}}><strong>Order Status:</strong> {order.orderCurrentStatus}</a><br />
+                      
                       <strong>Order Placed By:</strong> {order.orderPlacedBy}<br />
                       <strong>Order Placed On:</strong> {order.orderPlacesTime}<br />
                       <strong>Order Placed On:</strong> {order.orderReadyTime}<br />
@@ -60,6 +110,8 @@ function MyOrders(props) {
           }
         </div>
       </div>
+    </div>
+   
     </div>
   )
 }
