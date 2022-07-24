e791a616036f73827509e299c6ee25453a4bf64b
sureja <dh276903@dal.ca>
2022-07-24 02:01:00 -0300

    feedback backend

---
+++ feedback/feedbackSentimentCloudFunction.js
@@ -0,0 +1,49 @@
+
+import argparse
+
+from google.cloud import language_v1
+
+import six
+
+def sample_analyze_sentiment(content):
+
+    client = language_v1.LanguageServiceClient()
+
+    # content = 'Your text to analyze, e.g. Hello, world!'
+
+    if isinstance(content, six.binary_type):
+        content = content.decode("utf-8")
+
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    document = {"type_": type_, "content": content}
+
+    response = client.analyze_sentiment(request={"document": document})
+    sentiment = response.document_sentiment
+    print("Score: {}".format(sentiment.score))
+    print("Magnitude: {}".format(sentiment.magnitude))
+    return str(sentiment.score)
+
+
+def hello_world(request):
+    if request.method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    # return ('Hello World!', 200, headers)
+
+
+    request_json = request.get_json()
+    req = request_json['feedback']
+    return (sample_analyze_sentiment(req),200,headers)
+
+ 
