f7905cf84acefebe8fd2724d7a67a2d333cdeb6a
Krishna Jadav <krishnajadav78657@gmail.com>
2022-09-04 20:08:25 -0300

    Update create_tickets.py

--- tour-operator/create_tickets.py
+++ tour-operator/create_tickets.py
@@ -7,9 +7,9 @@ from google.cloud import pubsub_v1
 
 dynamodb_client = boto3.client('dynamodb',
                                region_name='us-east-1',
-                               aws_access_key_id="ASIARR4VPHN4S2OSS6PW",
-                               aws_secret_access_key="VDBXg3k2T3fYJ906Qncg9csDaS0JtTAO6gTSdxo5",
-                               aws_session_token="FwoGZXIvYXdzEFwaDDnw5pwtC2btgLyjgyLAASoXJCcb36tx8v0dfdHJLe9txJrslFg6bphh8uNnZYXPAkBO0ybvIbX/36LtjNpQnz/2O7CRwIpm9s0AC0A+mGbT27/UcBVJMcJRUv+BRH/MHqCjB7s0A32c/wq5MVPF7UWtJxeRgEvf4ytaT7ILZhNyko5RJA2vRPcdE1si/ezNTP7pcVYrFfXR3KPE6kj/P9XsOHx1HWJwvLhpQDIY98yB7z8JAeleaO0OpBy9rWEXuCNiB32MofXH8Yd76GXk8ijsqPaWBjItNaxfjtY2njQk3RkdHzzlY6IG4h7NW65q8cveaFT2xvbzH9afO1n67vkk5u/6")
+                               aws_access_key_id="",
+                               aws_secret_access_key="",
+                               aws_session_token="")
 
 
 TOUR_DETAILS_TABLE_NAME = "tour-details"
